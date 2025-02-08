1eb7c48eb4b90a374facc65c82ab414ecf630494
mdmasum4713 <mdmasum4713@gmail.com>
2025-02-08 20:57:59 +0600

    work complete

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,8 +1,213 @@
-import React from 'react'
- const WorkPage = () => {
-  return (
-    <div>WorkPage</div>
-  )
-}
+"use client";
 
-export default WorkPage
+import * as React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowUpRight, Github, ChevronLeft, ChevronRight } from "lucide-react";
+import { motion } from "framer-motion";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Separator } from "@/components/ui/separator";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Container from "@/components/Container";
+import projectOne from "@/public/images/projectOne (2).png";
+import projectTwo from "@/public/images/projectTwo.png";
+import projectThree from "@/public/images/projectThree.png";
+
+// Dummy data for projects
+const projects = [
+  {
+    id: "01",
+    title: "E-commerce Platform",
+    category: "Just Design",
+    description:
+      "A React project using Tailwind CSS, DaisyUI, and JavaScript for a responsive, stylish, and dynamic UI.",
+    stack: ["React", "Tailwind CSS", "DasyUi", "js"],
+    image: projectOne,
+    liveUrl: "https://unrivaled-gecko-066d73.netlify.app/",
+    githubUrl: "https://github.com/mdmasum4713/Online-Marketing-Client-Side",
+  },
+  {
+    id: "02",
+    title: "Task Management App",
+    category: "Frontend",
+    description:
+      "A sleek task management application that helps users organize their daily activities, set priorities, and track progress. Built with React and Redux for state management.",
+    stack: ["React", "Redux", "Styled Components", "Firebase"],
+    image: projectTwo,
+    liveUrl: "https://example-taskmanager.com",
+    githubUrl: "https://github.com/example/taskmanager",
+  },
+  {
+    id: "03",
+    title: "Just Design",
+    category: "Frontend",
+    description:
+      "A modern React project using Tailwind CSS and DaisyUI for a sleek, responsive, and user-friendly design.",
+    stack: ["React", "Tailwind CSS", "DasyUi",],
+    image: projectThree,
+    liveUrl: "https://cerulean-paletas-20c55b.netlify.app/#",
+    githubUrl: "https://github.com/mdmasum4713/About-Us",
+  },
+];
+
+export default function ProjectSlider() {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const handleSlideChange = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6 md:py-12"
+    >
+      <Container>
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="w-full"
+          onSelect={() => {
+            const index = 0;
+            if (typeof index === "number") {
+              handleSlideChange(index);
+            }
+          }}
+        >
+          <CarouselContent>
+            {projects.map((project) => (
+              <CarouselItem key={project.id}>
+                <Card className="bg-bodyColor border-lightSky/20">
+                  <CardContent className="p-6">
+                    <div className="flex flex-col md:flex-row md:items-center md:space-x-8">
+                      <div className="w-full md:w-1/2 order-2 md:order-1 mb-8 md:mb-0">
+                        <div className="space-y-3 md:space-y-6 mt-4 md:mt-0">
+                          <h2 className="text-4xl md:text-8xl leading-none font-extrabold text-transparent text-outline">
+                            {project?.id}
+                          </h2>
+                          <h3 className="text-xl md:text-3xl font-bold leading-none text-white group-hover:text-lightSky hoverEffect">
+                            {project?.category} project
+                          </h3>
+                          <p className="text-white/60 text-sm md:text-base leading-6 md:leading-normal">
+                            {project?.description}
+                          </p>
+                          <ul className="flex flex-wrap md:flex-nowrap gap-2 md:gap-4 items-center">
+                            {project?.stack?.map((item, index) => (
+                              <li
+                                key={index}
+                                className="text-xs md:text-base text-lightSky"
+                              >
+                                {item}
+                                {index !== project?.stack?.length - 1 && ","}
+                              </li>
+                            ))}
+                          </ul>
+                          <Separator className="bg-gray-700" />
+                          <div className="flex items-center space-x-4">
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <Link href={project.liveUrl}  target="_blank">
+                                    <Button
+                                      variant="outline"
+                                      size="icon"
+                                      className="bg-lightSky/5 text-white/80 border-lightSky/20 hover:bg-lightSky/10 hover:border-lightSky hover:text-hoverColor hoverEffect"
+                                    >
+                                      <ArrowUpRight className="h-4 w-4" />
+                                      <span className="sr-only">
+                                        View Live Project
+                                      </span>
+                                    </Button>
+                                  </Link>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>View Live Project</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <Link href={project.githubUrl} target="_blank" rel="noopener noreferrer">
+                                    <Button
+                                      variant="outline"
+                                      size="icon"
+                                      className="bg-lightSky/5 text-white/80 border-lightSky/20 hover:bg-lightSky/10 hover:border-lightSky hover:text-hoverColor hoverEffect"
+                                    >
+                                      <Github className="h-4 w-4" />
+                                      <span className="sr-only">View GitHub Repository</span>
+                                    </Button>
+                                  </Link>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>View GitHub Repository</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full md:w-1/2 order-1 md:order-2">
+                        <div className="relative h-64 md:h-96 bg-gray-700 rounded-lg overflow-hidden">
+                          <Image
+                            src={project.image}
+                            alt={project.title}
+                            fill
+                            className="object-cover"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="absolute right-10 -bottom-8 bg-red-600">
+            <CarouselPrevious className="hidden md:flex rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-5 hoverEffect" />
+            <CarouselNext className="hidden md:flex rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-5 hoverEffect" />
+          </div>
+        </Carousel>
+        <div className="flex justify-center mt-4 md:hidden">
+          <Button
+            variant="outline"
+            size="icon"
+            className="mr-2 rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-5 hoverEffect"
+            onClick={() => handleSlideChange(currentIndex - 1)}
+          >
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Previous slide</span>
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="mr-2 rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-5 hoverEffect"
+            onClick={() => handleSlideChange(currentIndex + 1)}
+          >
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Next slide</span>
+          </Button>
+        </div>
+      </Container>
+    </motion.section>
+  );
+}
